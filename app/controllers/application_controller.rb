9aee99ece9bd7575a6ac6404daad42ee686a5936
HelloRichard <richardhlmavs214@yahoo.com>
2016-09-15 17:12:28 +0000

    added name address and description to adata base

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   def index
+    @places = Place.all
   end
   protect_from_forgery with: :exception
 end
