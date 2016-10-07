d695a3558b19b4cdbc93c6ec9c25903e7cad16fc
HelloRichard <richardhlmavs214@yahoo.com>
2016-10-07 18:48:50 +0000

    update

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,4 +1,4 @@
 Geocoder.configure(
   :lookup => :bing,
-  :api_key => ENV['AIzaSyAheAKCX1n3OUTkknAulUgBOIjQ79d5RTU'],
+  :api_key => ENV['AZWOmVyP55U57GgYPiGB~nAHGZ2eN-F3GCG1osZK3UQ~AjnOD3D4WO8tD9IU0V98Nor9X-V4-19saDxcV-bVKN0B0atH4apmMomTtV-vBBA8'],
 )
