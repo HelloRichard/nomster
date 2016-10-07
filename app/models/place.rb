388e8e6c2271748c675ef544848cba861746b081
HelloRichard <richardhlmavs214@yahoo.com>
2016-10-07 19:49:32 +0000

    added a comment section

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,9 @@
 class Place < ActiveRecord::Base
   belongs_to :user
+  has_many :comments
   geocoded_by :address
   after_validation :geocode
-  
+
   validates :name, presence: true
   validates :name, length: {minimum: 2}
   validates :address, presence: true
